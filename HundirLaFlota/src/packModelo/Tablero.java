ae365688bbad8cc626f48488cc332dd14923f13e
Unai <Unai@10.109.124.226>
2017-03-22 17:50:51 +0100

    Actualizado Tablero

--- HundirLaFlota/src/packModelo/Tablero.java
+++ HundirLaFlota/src/packModelo/Tablero.java
@@ -7,64 +7,41 @@ import java.util.List;
 public class Tablero {
 	private static int ANCHO=10;
 	private static int ALTO=10;
-	
+
 	private List<TBarco> flota;
-	
+
 	public Tablero() {
 		flota= new ArrayList<TBarco>();
 	}
-	
+
 	public void addBarco (TBarco pBarco) {
 		flota.add(pBarco);
 	}
-	
+
 	public void eliminarBarco(TBarco pBarco) {
 		flota.remove(pBarco);
 	}
-	
+	private Iterator<TBarco> getIterador(){
+		return this.flota.iterator();
+	}
 	public boolean comprobarPosiciones(int pX,int pY) {
 		boolean libre = true;
 		Iterator <TBarco> it= flota.iterator();
-		TBarco b = it.next();
-		if(libre==true && b.esta(pX, pY)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX+1, pY)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX+1, pY-1)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX, pY-1)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX-1, pY-1)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX-1, pY)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX-1, pY+1)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX, pY+1)==true){
-			libre = false;
-		}
-		if(libre==true && b.esta(pX+1, pY+1)==true){
-			libre = false;
+		TBarco b;
+		while(it.hasNext()&&libre){
+			b=(TBarco)it.next;
+			for(int i=pX-1;pX<=pX+1;pX++){
+				for(int j=pY-1; pY<=pY+1;pY++){
+					if(j!=pY&&i!=pX){
+						if(b.esta(j,i)){
+							libre=false;
+							break;
+							break;
+						}
+					}
+				}
+			}
 		}
 		return libre;
 	}
-	
-	public void comprobarHorizontal(int pAlto) {
-		
-	}
-	
-	public void comprobarVertical(int pAncho) {
-		
-	}
-	
-	
-
-	
 }
