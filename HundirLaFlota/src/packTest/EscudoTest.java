08d76f050e58399fc87a3f081d76f936b7ce8208
dei <dei@dei-System-Product-Name>
2017-03-25 19:42:24 +0100

    Junit

---
+++ HundirLaFlota/src/packTest/EscudoTest.java
@@ -0,0 +1,14 @@
+package packTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EscudoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
